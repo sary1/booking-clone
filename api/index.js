1e68e67237d892056184a135569eee607880bb42
sary <arch.selmelegy@gmail.com>
2022-07-23 21:10:49 +0200

    Connect the database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app = express();
 dotenv.config();
+
+// DATABASE
+await mongoose
+  .connect(process.env.MONGO)
+  .then(() => console.log("Database connected .. Booking"))
+  .catch((err) => console.log(err));
 
 // MIDDLEWARES
 app.use(express.json());
