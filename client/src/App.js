0010e96f7ff29226b80ae6d5b09b7b49163b86c2
sary <arch.selmelegy@gmail.com>
2022-07-29 22:49:08 +0200

    Auth frontend and routing initialization

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Hotel from "./pages/hotel/Hotel";
 import List from "./pages/list/List";
+import Login from "./pages/login/Login.jsx";
+import Register from "./pages/Register/Register";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/hotels" element={<List />} />
         <Route path="/hotels/:id" element={<Hotel />} />
+        <Route path="/api/auth/login" element={<Login />} />
+        <Route path="/api/auth/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
