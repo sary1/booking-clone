936fa49d821f010e6c23f7b84abac37f3e7aa4ee
sary <arch.selmelegy@gmail.com>
2022-07-30 06:47:38 +0200

    Starting the User model

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,7 +1,14 @@
+import User from "../models/User.js";
+
 export const login = (req, res) => {
   res.status(200).json({ page: "login page" });
 };
 
-export const register = (req, res) => {
-  res.status(200).json({ page: "register page" });
+export const register = async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(200).json({ user });
+  } catch (error) {
+    res.status(400).json(error);
+  }
 };
---
+++ api/models/User.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+const UserSchema = new mongoose.Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    bookings_num: {
+      type: Number,
+      default: 0,
+    },
+    bookings: {
+      type: [String],
+    },
+    reviews: {
+      type: [String],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
