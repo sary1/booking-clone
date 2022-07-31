85d309392a9cfe7ba10381d1b28a04d7aa614e57
sary <arch.selmelegy@gmail.com>
2022-07-31 11:26:35 +0200

    Validate password and login user

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,4 +1,5 @@
 import User from "../models/User.js";
+import bcrypt from "bcrypt";
 
 const displayErrors = (error) => {
   const validationErrors = [];
@@ -9,13 +10,33 @@ const displayErrors = (error) => {
   return validationErrors;
 };
 
-export const login = (req, res) => {
-  res.status(200).json({ page: "login page" });
+export const login = async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(404).json({ error: "User not found" });
+    if (!req.body.password)
+      return res.status(400).json({ error: "Please provide your password" });
+
+    const validatePassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!validatePassword)
+      return res.status(400).json({ error: "Invalid Password" });
+
+    res.status(200).json({ user });
+  } catch (error) {
+    res.status(400).json({ error });
+  }
 };
 
 export const register = async (req, res) => {
   try {
-    const user = await User.create(req.body);
+    const user = new User({ ...req.body });
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+    await user.save();
+
     res.status(201).json({ user });
   } catch (error) {
     const errors = displayErrors(error);
