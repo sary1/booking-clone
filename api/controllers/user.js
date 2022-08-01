66326dee32d97fb94c4c05428b6baf36642119bb
sary <arch.selmelegy@gmail.com>
2022-08-01 20:18:21 +0200

    Fetching user from the database using the token data

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,11 +1,14 @@
 import _ from "lodash";
+import User from "../models/User.js";
 
-export const getUser = (req, res) => {
+export const getUser = async (req, res) => {
   try {
-    const user = req.user;
+    const user = await User.findById(req.user._id);
     if (!user) return res.status(404).json({ error: "User not found" });
 
-    res.status(200).json({ user: _.pick(user, ["_id", "isAdmin"]) });
+    res
+      .status(200)
+      .json({ user: _.omit(user.toObject(), ["_id", "password", "__v"]) });
   } catch (error) {
     res.status(400).json({ error });
   }
